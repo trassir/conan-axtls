eb1314afb02da7b01da68b67ed63f31d975e3f64
worldemar <woldemar@mimas.ru>
2019-11-22 13:11:34 +0300

    deploy in build.py

--- build.py
+++ build.py
@@ -2,10 +2,12 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(\
+    builder = ConanMultiPackager(
         username="dssl",
         channel='ci',
         build_policy='missing',
+        login_username='worldemar',
+        upload='https://api.bintray.com/conan/trassir/conan-public'
     )
 
     builder.add_common_builds()
